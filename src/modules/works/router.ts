9775be7c8607c159d12b0b079d949f2970d0a471
mary118111 <metpravda@gmail.com>
2023-10-03 12:12:07 +0200

    added works section

---
+++ src/modules/works/router.ts
@@ -0,0 +1,20 @@
+import module from './module.vue'
+import index from './views/index.vue'
+import paneLayout from '@/layouts/pane-layout.vue'
+
+export default {
+  path: '/works',
+  name: 'works',
+  component: module, // <-- necessary
+  meta: {
+    layout: paneLayout // <-- your layout, paneLayout if it is not set
+  },
+  children: [
+    // <-- your routes
+    {
+      path: '',
+      name: 'Работы',
+      component: index
+    }
+  ]
+}
