ba4f92459ce32b8f263dc2555c5b300959f42453
Mona Lisa <nukleoid@outlook.com>
2024-01-17 00:17:39 +0100

    minor fixes for build

--- src/modules/create-work/views/task-form-view.vue
+++ src/modules/create-work/views/task-form-view.vue
@@ -3,14 +3,13 @@
     <create-work-task-form
       v-if="taskSlug === 'new'"
       mode="create"
-      v-model="createWorkStore.taskToAdd"
+      v-model="(createWorkStore.taskToAdd as Task)"
       @submit="createWorkStore.addTask()"
     />
     <create-work-task-form
       v-else
       mode="update"
       v-model="createWorkStore.taskMap[taskSlug]"
-      @submit="createWorkStore.save()"
     />
   </div>
 </template>
@@ -20,6 +19,7 @@ import CreateWorkTaskForm from '@/modules/create-work/components/create-work-tas
 import { useCreateWorkStore } from '../stores/create-work'
 import { useRoute } from 'vue-router'
 import { computed } from 'vue'
+import type { Task } from '@/types/entities/Task'
 
 const createWorkStore = useCreateWorkStore()
 const route = useRoute()
