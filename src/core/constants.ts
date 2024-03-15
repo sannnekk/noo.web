05b141f38eeacab01feee1f19dd86f8b3040b985
Mona Lisa <nukleoid@outlook.com>
2024-03-15 10:23:33 +0100

    [FIX] Added https

--- src/core/constants.ts
+++ src/core/constants.ts
@@ -1,7 +1,7 @@
 export const Constants = {
   API_URL:
     import.meta.env.MODE === 'production'
-      ? 'http://www.api.noo-school.ru'
+      ? 'https://www.api.noo-school.ru'
       : 'http://localhost:3000',
   MEDIA_URL: 'https://www.cdn.noo-school.ru/uploads'
 } as const
