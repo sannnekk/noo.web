6c811b14af604b8b9efc09ccd884e38f42f16855
Mona Lisa <nukleoid@outlook.com>
2024-03-15 02:40:26 +0100

    [FIX] Fixed api url

--- src/core/constants.ts
+++ src/core/constants.ts
@@ -1,7 +1,7 @@
 export const Constants = {
   API_URL:
     import.meta.env.MODE === 'production'
-      ? 'https://api.noo-school.ru'
-      : 'https://api.noo-school.ru', //'http://localhost:3000',
-  MEDIA_URL: 'https://cdn.noo-school.ru/uploads'
+      ? 'https://www.api.noo-school.ru'
+      : 'http://localhost:3000',
+  MEDIA_URL: 'https://www.cdn.noo-school.ru/uploads'
 } as const
