097c266937fa480e4f3e66baf7d1f4e397249d48
sannnekk <nukleoid@outlook.com>
2025-10-20 08:24:27 +0200

    [IMPR] Deadline shift now by 5 days not 3

--- src/modules/assigned-works/components/single-work/work-actions.vue
+++ src/modules/assigned-works/components/single-work/work-actions.vue
@@ -153,7 +153,7 @@
     v-model:visible="assignedWorkStore.shiftDeadlineModalVisible"
     @confirm="assignedWorkStore.shiftDeadline()"
   >
-    <template #title> Сдвинуть дедлайн на 3 дня </template>
+    <template #title> Сдвинуть дедлайн на 5 дней </template>
     <template #text>
       Вы уверены, что хотите сдвинуть дедлайн?
       <br />
